888bcdbdee9ebeba9211876f2d17932527136459
piever <pietro.vertechi@protonmail.com>
2022-04-13 12:48:33 +0200

    add code to generate latex table

--- figures/benchmarks.jl
+++ figures/benchmarks.jl
@@ -2,6 +2,45 @@ using CSV
 
 df = CSV.File(joinpath(@__DIR__, "benchmarks.csv"))
 
+# Display formatted table
+
+function pretty_print(io, x::Number)
+    d, r = fldmod(x, 1)
+    r ≈ 0 ? print(io, Int(d)) : print(io, x)
+end
+
+function print_benchmarks(io, df)
+    print(io, raw"""
+    \begin{tabular}{llllll}
+        \hline
+        \textbf{machine} & \textbf{size} & \textbf{device} & \textbf{forward (\si{\milli\second})} & \textbf{backward (\si{\milli\second})} & \textbf{ratio} \\ \hline
+    """)
+    for row in df
+        print(io, "    ")
+        print(io, row.machine)
+        print(io, " & ")
+        print(io, row.problemsize)
+        print(io, " & ")
+        print(io, row.device)
+        print(io, " & ")
+        pretty_print(io, round(row.forward / 1000, sigdigits=5))
+        print(io, " & ")
+        pretty_print(io, round(row.backward / 1000, sigdigits=5))
+        print(io, " & ")
+        pretty_print(io, round(row.backward / row.forward, digits=3))
+        print(io, raw" \\ ")
+        print(io, '\n')
+    end
+    print(io, raw"""
+        \hline
+    \end{tabular}
+    """)
+end
+
+print_benchmarks(stdout, df)
+
+# Display figure
+
 using AlgebraOfGraphics, CairoMakie
 set_aog_theme!()
 
